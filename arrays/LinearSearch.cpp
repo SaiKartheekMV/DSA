0089aef59735afbe1f1af8486378c18b4a674e47
SaiKartheekMV <venkatasaikartheekmulukutla@gmail.com>
2026-01-08 21:26:51 +0530

    day 2 set 2

---
+++ arrays/LinearSearch.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int LinearSearch(int arr[], int n, int x){
+    for( int i = 0; i < n; i++){
+        if(arr[i]==x){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int x;
+    cin>>x;
+    int ans = LinearSearch(arr,n,x);
+    cout<<ans<<endl;
+    return 0;
+}
