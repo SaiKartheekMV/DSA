3eb8c8f562c25f0c0d02ad58298277d87a6b8819
SaiKartheekMV <venkatasaikartheekmulukutla@gmail.com>
2026-01-23 22:25:24 +0530

    Day 11 Linked List Started

---
+++ linked_list/Linked_list_basic.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class ListNode{
+    public:
+    int data;
+    ListNode* next;
+
+    ListNode(int val){
+        data = val;
+        next = NULL;
+
+    }
+};
+
+int main(){
+    int n;
+    cin >> n;
+    ListNode* head = NULL;
+    ListNode* tail = NULL;
+
+    for(int i=0; i<n; i++){
+        int x;
+        cin>>x;
+
+        ListNode* newNode = new ListNode(x);
+
+
+        if(head==NULL){
+            head = newNode;
+            tail = newNode;
+        }else{
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    ListNode* temp = head;
+    while(temp!= NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+
+    return 0;
+
+}
