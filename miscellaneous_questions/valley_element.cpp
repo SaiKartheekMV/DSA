2bb68a8074f9b48ac2c1fdaa10af61e8ac7bb9e2
SaiKartheekMV <venkatasaikartheekmulukutla@gmail.com>
2026-02-08 22:46:34 +0530

    Added miscellaneous problems

---
+++ miscellaneous_questions/valley_element.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    vector<int>ans;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int j=1,cnt=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]>arr[j]){
+            cnt+=1;
+            ans.push_back(arr[j]);
+        }
+        j+=1;
+    }
+
+    cout<<cnt<<endl;
+    for(int x:ans){
+        cout<<x<<" ";
+    }
+    return 0;
+}
