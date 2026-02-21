bcf907e231c1c90835d60ee87f40234899085523
SaiKartheekMV <venkatasaikartheekmulukutla@gmail.com>
2026-02-21 11:59:21 +0530

    Number of Recent calls

---
+++ stacks_and_queues/queue/recent_calls.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class RecentCounter{
+    private:
+        queue<int>q;
+    public:
+        RecentCounter(){
+
+        }
+        int ping(int t){
+            q.push(t);
+            while(!q.empty() && q.front()<t-3000){
+                q.pop();
+            }
+            return q.size();
+        }
+};
+
+int main(){
+    RecentCounter rc;
+    cout<<rc.ping(1)<<endl;
+    cout<<rc.ping(10)<<endl;
+    cout<<rc.ping(300)<<endl;
+    cout<<rc.ping(3002)<<endl;
+    return 0;
+}
