6d8bf255b646caedf7f8d346ac99bd7ddb9cf3e0
SaiKartheekMV <venkatasaikartheekmulukutla@gmail.com>
2026-03-02 21:48:30 +0530

    trapping rain water

---
+++ arrays/trapping_rain_water.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int rainWater(vector<int>&heights){
+    int left = 0;
+    int right = heights.size()-1;
+    int leftMax = 0;
+    int rightMax = 0;
+    int ans = 0;
+
+    while(left<right){
+        if(heights[left]<heights[right]){
+            if(heights[left]>=leftMax){
+                leftMax = heights[left];
+            }else{
+                ans+=leftMax-heights[left];
+            }
+            left++;
+        }else{
+            if(heights[right]>=rightMax){
+                rightMax = heights[right];
+            }else{
+                ans+=rightMax-heights[right];
+            }
+            right--;
+        }
+    }
+    return ans;
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>heights(n);
+    for(int i=0;i<n;i++){
+        cin>>heights[i];
+    }
+    int ans = rainWater(heights);
+    cout<<ans<<endl;
+    return 0;
+}
+
