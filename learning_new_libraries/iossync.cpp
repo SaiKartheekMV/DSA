5e2a84053b0dcf0970918619c565cf5fc779994e
SaiKartheekMV <venkatasaikartheekmulukutla@gmail.com>
2026-03-02 23:13:43 +0530

    new library for fast execution

---
+++ learning_new_libraries/iossync.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout<<"Make the Input and Output functions optimization and more faster";
+    return 0;
+}
