6f71666f12dd569e4161d3049c280a09b2888bec
SaiKartheekMV <venkatasaikartheekmulukutla@gmail.com>
2026-03-10 10:53:39 +0530

    Binary Search

---
+++ binarySearch/binarySearch.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int binarySearch(vector<int>&nums, int target){
+    int low = 0;
+    int high = nums.size()-1;
+
+    while(low<=high){
+        int mid = low + (high - low)/2;
+        if(nums[mid] == target){
+            return mid;
+        }else if(nums[mid]<target){
+            low = mid+1;
+        }else{
+            high = mid-1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, target;
+    cin >> n;
+    vector<int>nums(n);
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    cin>>target;
+    int ans = binarySearch(nums, target);
+    cout<<ans<<endl;
+    return 0;
+}
