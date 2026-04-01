18e10c5b9931ecd54ed5dc7f58b103349c28d94e
SaiKartheekMV <venkatasaikartheekmulukutla@gmail.com>
2026-04-01 23:38:04 +0530

    subarray product

---
+++ prefixsumHashing/subarrayProduct.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> subarrayProduct(vector<int>& nums){
+    int n = nums.size();
+    int right = 1;
+    vector<int> ans(n);
+
+    ans[0] = 1;
+    for(int i = 1; i < n; i++){
+        ans[i] = ans[i-1] * nums[i-1];
+    }
+
+    for(int i = n-1; i >= 0; i--){
+        ans[i] = ans[i] * right;
+        right = right * nums[i];
+    }
+    
+    return ans;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++){
+        cin >> nums[i];
+    }
+
+    vector<int> ans = subarrayProduct(nums);
+
+    for(int x : ans){
+        cout << x << " ";
+    }
+
+    return 0;
+}
