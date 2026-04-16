3f37016239240eb2084d23c6b4f3aed45081f581
SaiKartheekMV <venkatasaikartheekmulukutla@gmail.com>
2026-04-16 22:33:33 +0530

    change in KthSmallest node in BST

--- trees/kthSmallestNode.cpp
+++ trees/kthSmallestNode.cpp
@@ -13,3 +13,36 @@ struct TreeNode{
     }
 };
 
+int k1;
+int ans;
+
+void helper(TreeNode* root){
+    if(root==NULL) return ;
+    helper(root->left);
+    k1--;
+    if(k1==0){
+        ans = root->val;
+        return;
+    }
+    helper(root->right);
+}
+
+
+int kthSmallest(TreeNode* root, int k){
+    k1 = k;
+    helper(root);
+    return ans;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(1);
+    root->right = new TreeNode(4);
+    root->left->right = new TreeNode(2);
+    int k;
+    cin>>k;
+    cout<<kthSmallest(root, k)<<endl;
+    return 0;
+}
