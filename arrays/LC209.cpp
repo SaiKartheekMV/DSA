f5a6f6a447e199f5c42d1c7232a1f3d0a0783bf2
SaiKartheekMV <venkatasaikartheekmulukutla@gmail.com>
2026-08-11 21:46:23 +0530

    LC209

---
+++ arrays/LC209.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int>&nums, int n, int target){
+    int left = 0;
+    int sum  = 0;
+    int ans = INT_MAX;
+    for(int right=0;right<n;right++){
+        sum += nums[right];
+        while(sum >= target){
+            ans = min(ans, right-left+1);
+            sum-=nums[left];
+            left++;
+        }
+    }
+    return ans == INT_MAX ? -1 : ans;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, target;
+    cin>>n;
+    vector<int>nums(n);
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    cin>>target;
+    cout<<solve(nums, n, target)<<endl;
+    return 0;
+}
